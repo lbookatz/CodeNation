f6df10fb05763e4e499bebb1cf3c73726a640576
Your Name <lbookatz@hotmail.com>
2021-05-11 11:37:56 +0100

    website challange

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ const dataChecker = (String,rank) => {
     if (string == "codenation" && rank ==1){
         consol.log("both are correct");
     }
-    else if (codenation == 1){        
+    else if (string == "codenation" ){        
         consol.log("Sting is correct but rank is wrong");
     }
     else {
